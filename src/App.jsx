652e5f48008f593f3c2776fc66f5dd318b8a6e02
DaniLedes99 <daniela.e.ledesma99@gmail.com>
2024-03-07 23:58:09 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,8 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import Button from "./Button.jsx";
-import TaskCardRobot from "./TaskCardRobot.jsx";
 import TaskList from "./TaskList.jsx";
 import TaskForm from "./TaskForm.jsx";
 import "tailwindcss/tailwind.css";
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
-  const [mensajeEnviado, setMensajeEnviado] = useState("");
-  const [counter, setCounter] = useState(0);
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-    console.log(inputValue);
-  };
-
-  const handleEnviarClick = (e) => {
-    setMensajeEnviado(inputValue);
-    e.preventDefault();
-  };
-
-  const Posts = () => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  };
-  const handleContador = () => {
-    setCounter(counter + 1);
-  };
-
-  useEffect(() => {
-    console.log("render");
-  }, [counter]);
-
   return (
     <main className="bg-zinc-900 h-screen">
       <div className="container mx-auto p-10">
