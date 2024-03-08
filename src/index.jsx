8cb5384c8b43e480ea23b5e5c1faa7f02ce99e96
DaniLedes99 <daniela.e.ledesma99@gmail.com>
2024-03-07 23:54:08 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,21 +4,6 @@ import App from "./App.jsx";
 import { TaskContextProvider } from "./Context/TaskContext.jsx";
 import "./index.css";
 
-//rfce para crear componente//
-
-const user = [
-  {
-    id: 1,
-    name: "Ryan",
-    image: "https://robohash.org/user1",
-  },
-  {
-    id: 2,
-    name: "Joe",
-    image: "https://robohash.org/user2",
-  },
-];
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <TaskContextProvider>
     <App />
